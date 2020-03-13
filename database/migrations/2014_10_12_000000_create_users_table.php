12a4fd2f65ece5b4011e71b664812eea088b8333
psmever <psmever@gmail.com>
2020-03-13 11:37:59 +0900

    기본 타입 추가.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,14 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
+            $table->string('uuid', 50)->unique()->comment('사용자 uuid');
+            $table->string('type', 6)->default('A02001')->comment('사용자 타입');
+            $table->string('state', 6)->default('A10000')->comment('사용자 상태');
+            $table->string('level', 6)->default('A20000')->comment('사용자 레벨');
+
+            $table->enum('active', ['Y', 'N'])->default('Y')->comment('사용자 상태(정상인지 아닌지)');
+
             $table->timestamps();
         });
     }
