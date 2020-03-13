9905ca07855534c88be491d332261463ba0f845b
psmever <psmever@gmail.com>
2020-03-13 14:15:49 +0900

    기본 repository 위치 변경.

--- app/Repositories/BackendServiceProvider.php
+++ app/Repositories/BackendServiceProvider.php
@@ -8,8 +8,11 @@ class BackendServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(
-            'App\Repositories\TestRepositoryInterface',
-            'App\Repositories\TestRepository',
+            'App\Repositories\v1\TestRepositoryInterface',
+            'App\Repositories\v1\TestRepository',
+
+            'App\Repositories\v1\AuthRepositoryInterface',
+            'App\Repositories\v1\AuthRepository',
         );
     }
 }
