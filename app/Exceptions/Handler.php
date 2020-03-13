0ed174acc3b84c778232bbd15752c3ef3075d47e
psmever <psmever@gmail.com>
2020-03-13 14:13:51 +0900

    기본 예외처리 추가.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Support\Facades\Log;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,53 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        // return parent::render($request, $exception);
+        $logid = date('Ymdhis'); // 로그 고유값.
+
+    	//TODO: Exception Control
+	    if ($exception instanceof \PDOException)  // mysql Exception 따로 기록.
+	    {
+			$logMessage = "ID:{$logid} Code:{$exception->getCode()} Message:{$exception->getMessage()} File:{$exception->getFile()} Line:{$exception->getLine()}";
+			Log::channel('pdoexceptionlog')->error($logMessage);
+
+	    }
+	    else if ($exception instanceof \App\Exceptions\CustomException) // 기타 Exception
+	    {
+		    return $exception->render($request, $exception);
+	    }
+
+	    if ($this->isHttpException($exception)) {  // 일 반 웹 요청 일떄.
+
+		    if (view()->exists('errors.'.$exception->getStatusCode($exception)))
+		    {
+			    return response()->view('errors.'.$exception->getStatusCode($exception), [
+			    	'message' => config('app.debug') ? $exception->getMessage() : ''
+			    ], $exception->getStatusCode($exception));
+		    }
+
+		    return response()->view('errors.500', [
+			    'message' => config('app.debug') ? $exception->getMessage() : ''
+		    ], 500);
+	    }
+	    else
+	    {
+	    	// ajax 요청 일떄.
+		    if(config('app.debug'))
+		    {
+			    return response()->json([
+				    'error' => 'Exception Error.',
+				    'error_class' => get_class($exception),
+				    'error_message' => $exception->getMessage(),
+			    ], 500);
+		    }
+		    else
+		    {
+			    return response()->json([
+				    'error_message' => $exception->getMessage(),
+			    ], 500);
+		    }
+	    }
+
+	    return parent::render($request, $exception);
     }
 }
