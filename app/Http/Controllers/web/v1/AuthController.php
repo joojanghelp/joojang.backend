fe65ae2c10ddd821d2c57d55498451a394124c86
psmever <psmever@gmail.com>
2020-03-16 09:32:25 +0900

    회원 인증 컨트롤러 추가. (이메일 인증.)

--- app/Http/Controllers/web/v1/AuthController.php
+++ app/Http/Controllers/web/v1/AuthController.php
@@ -8,23 +8,15 @@ use App\Traits\EmailAuthTrait;
 
 class AuthController extends Controller
 {
-
     use EmailAuthTrait;
 
     public function email_auth(Request $request)
     {
 		$input = $request->only('code');
-
         $authCode = $input['code'];
-
-
         $taskResult = $this->webEmailAuthPageCheckTraitFunc($authCode);
-
-
-
         $viewData['state'] = $taskResult['state'];
         $viewData['message'] = (isset($taskResult['message']) && $taskResult['message']) ? $taskResult['message'] : '정상 처리 하였습니다.';
-
 
         return view('web.v1.auth/email_auth', $viewData);
     }
