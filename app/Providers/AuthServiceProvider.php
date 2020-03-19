461c5099cdc03649d631a3c277b4b7cf41ba7ed6
psmever <psmever@gmail.com>
2020-03-19 18:09:31 +0900

    access token 만료시간 1주일로 수정(테스트용).

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,7 +29,8 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
 
-        Passport::tokensExpireIn(Carbon::now()->addMinutes(1));
+        // Passport::tokensExpireIn(Carbon::now()->addMinutes());
+        Passport::tokensExpireIn(Carbon::now()->addDays(7));
 	    Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
     }
 }
